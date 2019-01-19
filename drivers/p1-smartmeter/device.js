2354a1a23addd1b4e675bb56fa26f05a89b50892
Leendert de Kok <koktaildotcom@hotmail.com>
2019-01-19 15:01:55 +0100

    update gas usage once an hour

--- drivers/p1-smartmeter/device.js
+++ drivers/p1-smartmeter/device.js
@@ -1,28 +1,47 @@
 
 const Homey = require('homey');
 
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+}
+
 class P1Device extends Homey.Device {
     onInit () {
         const device = this;
         Homey.on('update.data', function (data) {
 
-            console.log(data);
+            let now = new Date();
+            let update = device.getSetting('meter_gas_update_date');
+
+            // if not yet set update the setting
+            if (null === update) {
+                update = new Date();
+                device.setSettings({
+                    meter_gas_update_date: update
+                });
+            }
+
+            update.addHours(1);
 
             let gasCurrent = 0;
             let gasNew = 0;
             let gasChange = 0;
 
-            if (data.gas) {
-                if (data.gas.reading) {
-                    gasNew = Number(data.gas.reading);
+            if (update <= now) {
+                if (data.gas) {
+                    if (data.gas.reading) {
+                        gasNew = Number(data.gas.reading);
+                    }
+
+                    gasCurrent = Number(device.getCapabilityValue('meter_gas.consumed'));
+                    gasChange = (gasNew - gasCurrent);
                 }
 
-                gasCurrent = Number(device.getCapabilityValue('meter_gas.consumed'));
-                gasChange = (gasNew - gasCurrent);
+                device.setCapabilityValue('meter_gas.measure', gasChange);
             }
 
             device.setCapabilityValue('meter_gas.consumed', data.gas.reading);
-            device.setCapabilityValue('meter_gas.measure', gasChange);
             device.setCapabilityValue('measure_power.consumed', data.electricity.received.actual.reading * 1000);
             device.setCapabilityValue('measure_power.generated', data.electricity.delivered.actual.reading * 1000);
             device.setCapabilityValue('meter_power.consumed', data.electricity.received.tariff1.reading + data.electricity.received.tariff2.reading);
