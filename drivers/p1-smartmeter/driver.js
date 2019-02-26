af353a89dee3680e2c1cfd30da66848a1d34a77b
Lucas van der Have <lucasvdhave@gmail.com>
2019-02-26 14:34:37 +0100

    Refactor driver to register flow card triggers

--- drivers/p1-smartmeter/driver.js
+++ drivers/p1-smartmeter/driver.js
@@ -1,16 +1,42 @@
 const Homey = require('homey');
 
 class P1Driver extends Homey.Driver {
-    onPairListDevices (data, callback) {
+
+    onInit() {
+        this.registerFlowCards();
+    }
+
+    onPairListDevices(data, callback) {
         let devices = [
             {
                 "name": "P1 smart meter",
                 "data": {
-                    "id": "p1-smartmeter"
+                    "id": "p1-smartmeter-fork"
                 },
             },
         ];
         callback(null, devices);
+    }
+
+    registerFlowCards() {
+        this._flowTriggerMeasurePowerConsumedChanged = new Homey.FlowCardTriggerDevice('measure_power.consumed.changed')
+            .register()
+        this._flowTriggerMeterPowerConsumedChanged = new Homey.FlowCardTriggerDevice('meter_power.consumed.changed')
+            .register()
+    }
+
+    triggerMeasurePowerConsumedChangedFlow(device, tokens, state) {
+        this._flowTriggerMeasurePowerConsumedChanged
+            .trigger(device, tokens, state)
+            .then(this.log)
+            .catch(this.error)
+    }
+
+    triggerMeterPowerConsumedChangedFlow(device, tokens, state) {
+        this._flowTriggerMeterPowerConsumedChanged
+            .trigger(device, tokens, state)
+            .then(this.log)
+            .catch(this.error)
     }
 }
 
