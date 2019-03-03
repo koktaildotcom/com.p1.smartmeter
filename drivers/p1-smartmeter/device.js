b8d1f5171621e5944c871aa8a2b412d20ce774c3
Lucas van der Have <lucasvdhave@gmail.com>
2019-03-03 14:05:19 +0100

    Flow integration (#5)
    * Add english and dutch locale files
    Add settings page html
    
    * Add .idea/ folder to .gitignore
    
    * Register new flow triggers
    
    * Update locales
    
    * Update settings page
    
    * Refactor driver to register flow card triggers
    
    * Refactor device init for better code separation and add logic to trigger
    a flow card when a value has changed
    
    * Remove dev code
    
    * Increment app version, remove more dev code and add units to token descriptions
    
    * Add new triggers to app.json
    
    * Refactor and implement firing of new triggers
    
    * Fix incorrect locale keys
    
    * Fix typos
    
    * Add new translations
    
    * Fix floating point precision issues
    
    * Fix weird edge case where the last updated gas meter date caused an error
    
    * Add loading timeout settings to p1 driver
    
    * Add more fields to settings page
    
    * Add bootstrap css to assets folder

--- drivers/p1-smartmeter/device.js
+++ drivers/p1-smartmeter/device.js
@@ -1,54 +1,110 @@
-
 const Homey = require('homey');
 
 class P1Device extends Homey.Device {
 
-    round (number) {
+    round(number) {
         return Math.round(number * 100) / 100
     }
 
-    onInit () {
-        const device = this;
+    onInit() {
+        let device = this;
+        device._driver = this.getDriver();
+        device.registerEventListeners(device);
+    }
+
+    registerEventListeners(device) {
         Homey.on('update.data', function (data) {
-
-            let now = new Date();
-            let update = device.getSetting('meter_gas_update_date');
-            let updateDate = null;
-
-            if (null === update) {
-                updateDate = new Date();
-                device.setSettings({
-                    meter_gas_update_date: updateDate.toISOString()
-                });
-            }
-            else{
-                // if not yet set update the setting
-                updateDate = new Date(update);
-            }
-
-            let gasCurrent = 0;
-            let gasNew = 0;
-            let gasChange = 0;
-
-            if (updateDate < new Date(now.getTime() - (1000*60*60))) {
-                if (data.gas) {
-                    if (data.gas.reading) {
-                        gasNew = Number(data.gas.reading);
-                    }
-
-                    gasCurrent = Number(device.getCapabilityValue('meter_gas.consumed'));
-                    gasChange = (gasNew - gasCurrent);
-                }
-
-                device.setCapabilityValue('meter_gas.measure', gasChange);
-            }
-
-            device.setCapabilityValue('meter_gas.consumed', device.round(data.gas.reading));
-            device.setCapabilityValue('measure_power.consumed', device.round(data.electricity.received.actual.reading * 1000));
-            device.setCapabilityValue('measure_power.generated', device.round(data.electricity.delivered.actual.reading * 1000));
-            device.setCapabilityValue('meter_power.consumed', device.round(data.electricity.received.tariff1.reading + data.electricity.received.tariff2.reading));
-            device.setCapabilityValue('meter_power.generated', device.round(data.electricity.delivered.tariff1.reading + data.electricity.delivered.tariff2.reading));
+            device.processData(data)
         });
+    }
+
+    processData(data) {
+        let device = this;
+        let now = new Date();
+        let update = device.getSetting('meter_gas_update_date');
+        let updateDate = null;
+
+        if (null === update) {
+            updateDate = new Date();
+            device.setSettings({
+                meter_gas_update_date: updateDate.getTime()
+            });
+        } else {
+            // if not yet set update the setting
+            updateDate = new Date(update);
+        }
+
+        let gasCurrent = 0;
+        let gasNew = 0;
+        let gasChange = 0;
+
+        if (updateDate < new Date(now.getTime() - (1000 * 60 * 60))) {
+            if (data.gas) {
+                if (data.gas.reading) {
+                    gasNew = Number(data.gas.reading) * 1000;
+                }
+                gasCurrent = Number(device.getCapabilityValue('meter_gas.consumed')) * 1000;
+
+                gasChange = (gasNew - gasCurrent) / 1000;
+
+                if (gasChange > 0) {
+                    device.updateCapabilityValue('meter_gas.measure', gasChange);
+
+                    device.setSettings({
+                        meter_gas_update_date: now.getTime()
+                    });
+                }
+            }
+        }
+
+        device.updateCapabilityValue('meter_gas.consumed', device.round(data.gas.reading));
+        device.updateCapabilityValue('measure_power.consumed', device.round(data.electricity.received.actual.reading * 1000));
+        device.updateCapabilityValue('measure_power.generated', device.round(data.electricity.delivered.actual.reading * 1000));
+        device.updateCapabilityValue('meter_power.consumed', device.round(data.electricity.received.tariff1.reading + data.electricity.received.tariff2.reading));
+        device.updateCapabilityValue('meter_power.generated', device.round(data.electricity.delivered.tariff1.reading + data.electricity.delivered.tariff2.reading));
+    }
+
+    updateCapabilityValue(capability, value) {
+        let device = this,
+            state = {},
+            currentValue = device.getCapabilityValue(capability);
+
+        device.setCapabilityValue(capability, value);
+
+        if (value !== currentValue) {
+            switch (capability) {
+                case 'measure_power.consumed':
+                    device._driver.triggerMeasurePowerConsumedChangedFlow(device, {
+                        "measure_power.consumed": value
+                    }, state);
+                    break;
+                case 'meter_power.consumed':
+                    device._driver.triggerMeterPowerConsumedChangedFlow(device, {
+                        "meter_power.consumed": value
+                    }, state);
+                    break;
+                case 'measure_power.generated':
+                    device._driver.triggerMeasurePowerGeneratedChangedFlow(device, {
+                        "measure_power.generated": value
+                    }, state);
+                    break;
+                case 'meter_power.generated':
+                    device._driver.triggerMeterPowerGeneratedChangedFlow(device, {
+                        "meter_power.generated": value
+                    }, state);
+                    break;
+                case 'meter_gas.measure':
+                    device._driver.triggerMeasureGasChangedFlow(device, {
+                        "meter_gas.measure": value
+                    }, state);
+                    break;
+                case 'meter_gas.consumed':
+                    device._driver.triggerMeterGasChangedFlow(device, {
+                        "meter_gas.consumed": value
+                    }, state);
+                    break;
+            }
+        }
     }
 }
 
--- drivers/p1-smartmeter/driver.js
+++ drivers/p1-smartmeter/driver.js
@@ -1,7 +1,16 @@
 const Homey = require('homey');
 
 class P1Driver extends Homey.Driver {
-    onPairListDevices (data, callback) {
+
+    onInit() {
+        this._flowTriggers = [];
+        this._flowTriggeredAt = [];
+        this._flowTriggerTimeouts = [];
+        this.registerFlowCards();
+        this.loadFlowTriggerTimeouts();
+    }
+
+    onPairListDevices(data, callback) {
         let devices = [
             {
                 "name": "P1 smart meter",
@@ -11,6 +20,112 @@ class P1Driver extends Homey.Driver {
             },
         ];
         callback(null, devices);
+    }
+
+    loadFlowTriggerTimeouts() {
+        let timeoutSettings = [
+            'measure_power.consumed.changed',
+            'meter_power.consumed.changed',
+            'measure_power.generated.changed',
+            'meter_power.generated.changed',
+            'meter_gas.measure.changed',
+            'meter_gas.consumed.changed'
+        ];
+
+        for (var i in timeoutSettings) {
+            let timeout = Number(Homey.ManagerSettings.get(timeoutSettings[i].replace('changed', 'timeout')));
+
+            if (timeout !== null) {
+                this._flowTriggerTimeouts[timeoutSettings[i]] = timeout;
+            } else {
+                this._flowTriggerTimeouts[timeoutSettings[i]] = 1;
+            }
+        }
+
+        console.log('Loaded settings', this._flowTriggerTimeouts);
+
+        let driver = this;
+
+        Homey.ManagerSettings.on('set', function (key) {
+            console.log('Setting "' + key + '" updated to "' + Homey.ManagerSettings.get(key) + '"');
+            driver._flowTriggerTimeouts[key.replace('timeout', 'changed')] = Homey.ManagerSettings.get(key);
+        });
+    }
+
+    getFlowTriggerTimeout(triggerName) {
+        if (triggerName in this._flowTriggerTimeouts) {
+            return this._flowTriggerTimeouts[triggerName];
+        }
+
+        throw new Error('timeout setting not found by key "' + triggerName + '"');
+    }
+
+    registerFlowCards() {
+        let triggers = [
+            'measure_power.consumed.changed',
+            'meter_power.consumed.changed',
+            'measure_power.generated.changed',
+            'meter_power.generated.changed',
+            'meter_gas.measure.changed',
+            'meter_gas.consumed.changed'
+        ];
+
+        for (let i in triggers) {
+            this._flowTriggers[triggers[i]] = new Homey.FlowCardTriggerDevice(triggers[i]).register();
+        }
+    }
+
+    triggerChangedFlow(triggerName, device, tokens, state) {
+        if (triggerName in this._flowTriggers && this.shouldTriggerFlow(triggerName)) {
+            this._flowTriggers[triggerName]
+                .trigger(device, tokens, state)
+                .then(this.log)
+                .catch(this.error);
+
+            this.updateFlowTriggeredAt(triggerName);
+        }
+    }
+
+    updateFlowTriggeredAt(triggerName) {
+        this._flowTriggeredAt[triggerName] = new Date();
+    }
+
+    shouldTriggerFlow(triggerName) {
+        let lastTriggeredAt = this._flowTriggeredAt[triggerName];
+
+        if (typeof lastTriggeredAt === 'undefined') {
+            return true;
+        }
+
+        let timeoutInSeconds = this.getFlowTriggerTimeout(triggerName),
+            shouldNotRunBefore = new Date(lastTriggeredAt.getTime() + Number(timeoutInSeconds)),
+            now = new Date();
+
+        return shouldNotRunBefore < now;
+    }
+
+    triggerMeasurePowerConsumedChangedFlow(device, tokens, state) {
+        this.triggerChangedFlow('measure_power.consumed.changed', device, tokens, state);
+    }
+
+    triggerMeterPowerConsumedChangedFlow(device, tokens, state) {
+        this.triggerChangedFlow('meter_power.consumed.changed', device, tokens, state);
+    }
+
+    triggerMeasurePowerGeneratedChangedFlow(device, tokens, state) {
+        this.triggerChangedFlow('measure_power.generated.changed', device, tokens, state);
+    }
+
+    triggerMeterPowerGeneratedChangedFlow(device, tokens, state) {
+        this.triggerChangedFlow('meter_power.generated.changed', device, tokens, state);
+    }
+
+    triggerMeasureGasChangedFlow(device, tokens, state) {
+        this.triggerChangedFlow('meter_gas.measure.changed', device, tokens, state);
+    }
+
+    triggerMeterGasChangedFlow(device, tokens, state) {
+        this.triggerChangedFlow('meter_gas.consumed.changed', device, tokens, state);
     }
 }
 
