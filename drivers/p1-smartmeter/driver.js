5b5f27668c51be8da19f21631f5e0dbf9fbd775c
Leendert de Kok <koktaildotcom@hotmail.com>
2019-03-24 18:03:27 +0100

    removed timeout from the code

--- drivers/p1-smartmeter/driver.js
+++ drivers/p1-smartmeter/driver.js
@@ -4,10 +4,7 @@ class P1Driver extends Homey.Driver {
 
     onInit() {
         this._flowTriggers = [];
-        this._flowTriggeredAt = [];
-        this._flowTriggerTimeouts = [];
         this.registerFlowCards();
-        this.loadFlowTriggerTimeouts();
     }
 
     onPairListDevices(data, callback) {
@@ -22,41 +19,6 @@ class P1Driver extends Homey.Driver {
         callback(null, devices);
     }
 
-    loadFlowTriggerTimeouts() {
-        let timeoutSettings = [
-            'measure_power.consumed.changed',
-            'meter_power.consumed.changed',
-            'measure_power.generated.changed',
-            'meter_power.generated.changed',
-            'meter_gas.measure.changed',
-            'meter_gas.consumed.changed'
-        ];
-
-        for (var i in timeoutSettings) {
-            let timeout = Number(Homey.ManagerSettings.get(timeoutSettings[i].replace('changed', 'timeout')));
-
-            if (timeout !== null) {
-                this._flowTriggerTimeouts[timeoutSettings[i]] = timeout;
-            } else {
-                this._flowTriggerTimeouts[timeoutSettings[i]] = 1;
-            }
-        }
-
-        let driver = this;
-
-        Homey.ManagerSettings.on('set', function (key) {
-            driver._flowTriggerTimeouts[key.replace('timeout', 'changed')] = Homey.ManagerSettings.get(key);
-        });
-    }
-
-    getFlowTriggerTimeout(triggerName) {
-        if (triggerName in this._flowTriggerTimeouts) {
-            return this._flowTriggerTimeouts[triggerName];
-        }
-
-        throw new Error('timeout setting not found by key "' + triggerName + '"');
-    }
-
     registerFlowCards() {
         let triggers = [
             'measure_power.consumed.changed',
@@ -67,38 +29,18 @@ class P1Driver extends Homey.Driver {
             'meter_gas.consumed.changed'
         ];
 
-        for (let i in triggers) {
-            this._flowTriggers[triggers[i]] = new Homey.FlowCardTriggerDevice(triggers[i]).register();
+        for (const trigger of triggers) {
+            this._flowTriggers[trigger] = new Homey.FlowCardTriggerDevice(trigger).register();
         }
     }
 
     triggerChangedFlow(triggerName, device, tokens, state) {
-        if (triggerName in this._flowTriggers && this.shouldTriggerFlow(triggerName)) {
+        if (triggerName in this._flowTriggers) {
             this._flowTriggers[triggerName]
                 .trigger(device, tokens, state)
                 .then(this.log)
                 .catch(this.error);
-
-            this.updateFlowTriggeredAt(triggerName);
         }
-    }
-
-    updateFlowTriggeredAt(triggerName) {
-        this._flowTriggeredAt[triggerName] = new Date();
-    }
-
-    shouldTriggerFlow(triggerName) {
-        let lastTriggeredAt = this._flowTriggeredAt[triggerName];
-
-        if (typeof lastTriggeredAt === 'undefined') {
-            return true;
-        }
-
-        let timeoutInSeconds = this.getFlowTriggerTimeout(triggerName),
-            shouldNotRunBefore = new Date(lastTriggeredAt.getTime() + Number(timeoutInSeconds)),
-            now = new Date();
-
-        return shouldNotRunBefore < now;
     }
 
     triggerMeasurePowerConsumedChangedFlow(device, tokens, state) {
