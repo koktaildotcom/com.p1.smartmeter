d76ff6e97bd4df8c1ec0c4b4aab04d17cd8056ff
Stephan Eizinga <stephan@monkeysoft.nl>
2020-05-05 12:46:39 +0200

    add support for send directrly an telegram message

--- api.js
+++ api.js
@@ -15,4 +15,19 @@ module.exports = [
             return callback( new Error('Cannot find body.') );
         },
     },
+    {
+        method: 'POST',
+        path: '/update/dsmrreader',
+        public: true,
+        fn: function (args, callback) {
+            if (args.hasOwnProperty('body')) {
+                const DsmrReader = Homey.app.dsmrreader;
+                Homey.emit('update.dsmrreaderdata', DsmrReader.parseData(args.body));
+
+                callback(null, 'OK');
+            }
+
+            return callback( new Error('Cannot find body.') );
+        },
+    },
 ];
